ea4443191bd7fc3f7ffa2c54aece8b749a5283f7
OChebanenko <acebanenco@gmail.com>
2022-12-16 05:42:06 +0100

    Java service and app to call rust library

--- rust_from_java/src/main/java/com/acebanenco/rust/RustFromJavaApp.java
+++ rust_from_java/src/main/java/com/acebanenco/rust/RustFromJavaApp.java
@@ -1,2 +1,14 @@
-package com.acebanenco.rust;public class RustFromJavaApp {
+package com.acebanenco.rust;
+
+// from https://blog.frankel.ch/start-rust/7/
+public class RustFromJavaApp {
+
+    public static void main(String[] args) {
+        RustFromJavaService service = new RustFromJavaService(2);
+        int doubleRust = service.doubleRust(2);
+        System.out.println("doubleRust(2) = " + doubleRust);
+
+        int timesRust = service.timesRust(3);
+        System.out.println("timesRust(3) = " + timesRust);
+    }
 }
